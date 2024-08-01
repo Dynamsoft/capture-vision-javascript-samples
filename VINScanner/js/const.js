34a91972fe49444333ac732d62359a4bb3807984
felixindrawan <indrawan.felix123@gmail.com>
2024-08-01 14:03:24 -0700

    feat: ui enhancement, update library

---
+++ VINScanner/js/const.js
@@ -0,0 +1,57 @@
+// Define some global variables that will be used
+let cameraList = [];
+let cameraView = null;
+let cvRouter = null;
+let cameraEnhancer = null;
+let pInit = null;
+let isSoundOn = true;
+let timer = null;
+
+const SCAN_MODES = ["barcode", "text", "both"];
+const SCAN_TEMPLATES = {
+  barcode: "ReadVINBarcode",
+  text: "ReadVINText",
+  both: "ReadVIN",
+};
+const SCAN_MODE_TITLES = {
+  barcode: "Scan by Barcode",
+  text: "Scan by Text",
+  both: "Scan Text and Barcode",
+};
+let currentMode = SCAN_MODES[2]; // Set scan mode as "Scan Both" by default
+
+// Get the UI element
+const homePage = document.querySelector(".home-page");
+
+const cameraViewContainer = document.querySelector(".camera-view-container");
+
+const scanBothBtn = document.querySelector("#scan-both-btn");
+
+const cameraListContainer = document.querySelector(".camera-list");
+const cameraSelector = document.querySelector(".camera-selector");
+
+const scannerContainer = document.querySelector(".scanner-container");
+const startScanningBtn = document.querySelector(".start-btn");
+
+const scanModeContainer = document.querySelector(".scan-mode-container");
+const scanMode = document.querySelector(".scan-mode");
+const settingsModal = document.querySelector(".settings-modal");
+const closeSettingsBtn = document.querySelector(".close-settings-btn");
+
+const resultContainer = document.querySelector(".result-container");
+const parsedResultArea = document.querySelector(".parsed-result-area");
+const parsedResultMain = document.querySelector(".parsed-result-main");
+const resultImageContainer = document.querySelector("#result-image-container");
+
+const copyResultBtn = document.querySelector(".copy-result-btn");
+const saveImageBtn = document.querySelector(".save-image-btn");
+
+const resultRestartBtn = document.querySelector(".result-restart");
+const restartVideoBtn = document.querySelector(".btn-restart-video");
+
+const playSoundBtn = document.querySelector(".music");
+const closeSoundBtn = document.querySelector(".no-music");
+const down = document.querySelector(".down");
+const up = document.querySelector(".up");
+
+const notification = document.querySelector("#notification");
---
+++ VINScanner/js/util.js
@@ -0,0 +1,118 @@
+/* Extracts and returns VIN details from the parsed VIN string
+ *
+ * @param {Object} result - The parsed result object containing VIN details.
+ * @returns {Object} An object with key-value pairs of the extracted fields.
+ */
+export function extractVinDetails(result) {
+  const parseResultInfo = {};
+
+  if (!result.exception) {
+    parseResultInfo["Region"] = result.getFieldValue("region");
+    parseResultInfo["Check Digit"] = result.getFieldValue("checkDigit");
+    parseResultInfo["Model Year"] = result.getFieldValue("modelYear");
+    parseResultInfo["Plant Code"] = result.getFieldValue("plantCode");
+    parseResultInfo["VIS"] = result.getFieldValue("VIS");
+  }
+
+  return parseResultInfo;
+}
+
+/**
+ * Create an HTML paragraph element containing the document field name and value.
+ *
+ * @param {string} field - The document field name.
+ * @param {string} value - The document field value.
+ * @returns {HTMLElement} The paragraph element containing the formatted document field name and value.
+ */
+export function resultToHTMLElement(field, value) {
+  const p = document.createElement("p");
+  p.className = "parsed-filed";
+  const spanFieldName = document.createElement("span");
+  spanFieldName.className = "field-name";
+  const spanValue = document.createElement("span");
+  spanValue.className = "field-value";
+
+  spanFieldName.innerText = `${field} : `;
+  spanValue.innerText = `${value || "Not detected"}`;
+
+  p.appendChild(spanFieldName);
+  p.appendChild(spanValue);
+
+  return p;
+}
+
+/**
+ * Checks and returns the current screen orientation.
+ *
+ * @returns {string} The current screen orientation ('portrait' or 'landscape').
+ */
+export function checkOrientation() {
+  if (window.matchMedia("(orientation: portrait)").matches) {
+    return "portrait";
+  } else if (window.matchMedia("(orientation: landscape)").matches) {
+    return "landscape";
+  }
+}
+
+export function getVisibleRegionOfVideo() {
+  if (!cameraView || !cameraView.getVideoElement()) return;
+  const video = cameraView.getVideoElement();
+  const { videoWidth, videoHeight } = video;
+  const objectFit = cameraView.getVideoFit();
+
+  // Adjust dimensions based on orientation
+  const isPortrait = checkOrientation() === "portrait";
+  const width = isPortrait ? Math.min(videoWidth, videoHeight) : Math.max(videoWidth, videoHeight);
+  const height = isPortrait ? Math.max(videoWidth, videoHeight) : Math.min(videoWidth, videoHeight);
+
+  // Get the CSS dimensions of the video element
+  const { width: videoCSSWidth, height: videoCSSHeight } = cameraView._innerComponent.getBoundingClientRect();
+  if (videoCSSWidth <= 0 || videoCSSHeight <= 0) {
+    throw new Error(`Unable to get video dimensions. Video may not be rendered on the page.`);
+  }
+
+  const videoCSSWHRatio = videoCSSWidth / videoCSSHeight,
+    videoWHRatio = width / height;
+  let cssScaleRatio;
+
+  // Set visible region in pixels
+  const regionInPixels = {
+    x: 0,
+    y: 0,
+    width: width,
+    height: height,
+    isMeasuredInPercentage: false,
+  };
+
+  if (objectFit === "cover") {
+    if (videoCSSWHRatio < videoWHRatio) {
+      // a part of length is invisible
+      cssScaleRatio = videoCSSHeight / height;
+      regionInPixels.x = Math.floor((width - videoCSSWidth / cssScaleRatio) / 2);
+      regionInPixels.y = 0;
+      regionInPixels.width = Math.ceil(videoCSSWidth / cssScaleRatio);
+      regionInPixels.height = height;
+    } else {
+      // a part of height is invisible
+      cssScaleRatio = videoCSSWidth / width;
+      regionInPixels.x = 0;
+      regionInPixels.y = Math.floor((height - videoCSSHeight / cssScaleRatio) / 2);
+      regionInPixels.width = width;
+      regionInPixels.height = Math.ceil(videoCSSHeight / cssScaleRatio);
+    }
+  }
+  return regionInPixels;
+}
+
+/** Show notification banner to users
+ * @params {string} message - noficiation message
+ * @params {string} className - CSS class name to show notification status
+ */
+export function showNotification(message, className) {
+  notification.className = className;
+  notification.innerText = message;
+  notification.style.opacity = 1;
+  setTimeout(() => {
+    notification.style.opacity = 0;
+  }, 2000);
+}
