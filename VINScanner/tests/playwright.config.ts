c16f3de29152b3afaec089213d94b8caafaa00ac
felixindrawan <indrawan.felix123@gmail.com>
2024-08-26 09:37:45 -0700

    fix: json

--- VINScanner/tests/playwright.config.ts
+++ VINScanner/tests/playwright.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './',
+  testDir: "./",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -11,7 +11,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -19,53 +19,51 @@ export default defineConfig({
 
     headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
         launchOptions: {
           args: [
-            '--disable-web-security',
-            '--enable-web-rtc',
-            '--use-fake-ui-for-media-stream',
-            '--use-fake-device-for-media-stream'
-          ]          
+            "--disable-web-security",
+            "--enable-web-rtc",
+            "--use-fake-ui-for-media-stream",
+            "--use-fake-device-for-media-stream",
+          ],
         },
         contextOptions: {
           /* Camera permission */
-          permissions: ['camera'],
-          ignoreHTTPSErrors: true
+          permissions: ["camera"],
+          ignoreHTTPSErrors: true,
         },
       },
     },
     {
-      name: 'firefox',
+      name: "firefox",
       use: {
-        ...devices['Desktop Firefox'],
+        ...devices["Desktop Firefox"],
         launchOptions: {
-          args: [
-            '--use-fake-device-for-media-stream',
-            '--use-fake-ui-for-media-stream'
-          ],
+          args: ["--use-fake-device-for-media-stream", "--use-fake-ui-for-media-stream"],
           firefoxUserPrefs: {
-            'permissions.default.camera': 1, // Allow camera access automatically
-            'media.navigator.streams.fake': true // Use fake streams if needed
-          }
-        }
-      }
+            "permissions.default.camera": 1, // Allow camera access automatically
+            "media.navigator.streams.fake": true, // Use fake streams if needed
+          },
+        },
+      },
     },
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'],
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
         launchOptions: {
-          args: ['--disable-web-security',
-            '--enable-web-rtc'
-          ]          
+          args: ["--disable-web-security", "--enable-web-rtc"],
         },
-       },
+      },
     },
+  ],
 });
