07d826de8eeef470ea17c2d127f5e89e18b77c5a
felixindrawan <indrawan.felix123@gmail.com>
2024-08-27 15:24:20 -0700

    fix: added camera options

--- VINScanner/playwright.config.ts
+++ VINScanner/playwright.config.ts
@@ -11,68 +11,48 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: "http://localhost:3000",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-  },
-
-  /* Configure projects for major browsers */
+  testDir: "./tests" /* Configure projects for major browsers */,
   projects: [
     {
-      name: "VINScanner - chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: [
+            "--disable-web-security",
+            "--enable-web-rtc",
+            "--use-fake-ui-for-media-stream",
+            "--use-fake-device-for-media-stream",
+          ],
+        },
+        contextOptions: {
+          /* Camera permission */
+          permissions: ["camera"],
+          ignoreHTTPSErrors: true,
+        },
+      },
     },
-
     {
-      name: "VINScanner - firefox",
-      use: { ...devices["Desktop Firefox"] },
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        launchOptions: {
+          args: ["--use-fake-device-for-media-stream", "--use-fake-ui-for-media-stream"],
+          firefoxUserPrefs: {
+            "permissions.default.camera": 1, // Allow camera access automatically
+            "media.navigator.streams.fake": true, // Use fake streams if needed
+          },
+        },
+      },
     },
-
     {
-      name: "VINScanner - webkit",
-      use: { ...devices["Desktop Safari"] },
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+        launchOptions: {
+          args: ["--disable-web-security", "--enable-web-rtc"],
+        },
+      },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
