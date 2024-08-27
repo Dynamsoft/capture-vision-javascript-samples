6e13999b6da49af9c36c24bde7decbb078fa6a81
felixindrawan <indrawan.felix123@gmail.com>
2024-08-27 14:59:56 -0700

    fix: get title

--- VINScanner/tests/example.spec.ts
+++ VINScanner/tests/example.spec.ts
@@ -21,5 +21,5 @@ test("load local page", async ({ page }) => {
   await page.goto("/VINScanner/index.html");
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.title()).toContain("VIN Scanner");
+  await expect(await page.title()).toContain("VIN Scanner");
 });
