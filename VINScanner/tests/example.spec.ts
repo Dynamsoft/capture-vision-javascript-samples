96625dd4a0090523d8229e418109fac907fdf1ce
felixindrawan <indrawan.felix123@gmail.com>
2024-08-27 13:57:13 -0700

    draft: playwright test

---
+++ VINScanner/tests/example.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://localhost:8080/VINScanner/index.html");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+});
+
+test("load local page", async ({ page }) => {
+  await page.goto("https://localhost:8080/VINScanner/index.html");
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.title()).toContain("VIN Scanner");
+});
