4496156f5c51def2bf7813e06531c7725263e0a4
Eugene Yu <Eugene.Yu@dynamsoft.com>
2024-08-28 10:14:59 -0700

    Updated the testDir to point to VINSCanner/tests folder. Change all page.goto path to local file since baseURL is defined in the playwright config setting already.

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./",
+  testDir: "./VINScanner/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,7 +26,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
-
+    /* Enable headless mode to see the test execution in live */
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
