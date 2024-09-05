94b9312a32ebb122768ce0e41df0658f183fc326
felixindrawan <indrawan.felix123@gmail.com>
2024-09-05 10:22:18 -0700

    fix: button switches

--- VINScanner/js/const.js
+++ VINScanner/js/const.js
@@ -13,11 +13,7 @@ const SCAN_TEMPLATES = {
   text: "ReadVINText",
   both: "ReadVIN",
 };
-const SCAN_MODE_TITLES = {
-  barcode: "Scan by Barcode",
-  text: "Scan by Text",
-  both: "Scan Text or Barcode",
-};
+
 let currentMode = SCAN_MODES[2]; // Set scan mode as "Scan Both" by default
 
 let scanOrientation = "landscape";
