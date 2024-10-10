4a4071e09fc43b185e476d15405a13491cf06f3d
felixindynamsoft <felixindynamsoft@users.noreply.github.com>
2024-10-10 20:21:49 +0000

    Remove preview for PR 20 🛬

--- pr-preview/pr-20/VINScanner/tests/minimum-elements.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("load local page", async ({ page }) => {
-  await page.goto("/VINScanner/minimum-elements.html");
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(await page.title()).toContain("VIN Scanner - Minimum elements");
-});
